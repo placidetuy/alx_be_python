8e8ef794e82ed4cb31a0838e94c0d59d89932c2e
“Placide” <mugahiplacide@gmail.com>
2024-09-16 02:27:48 -0700

    Add 0-iam_betty and 1-who_am_i scripts

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Assigning values to variables
+number1 = 10
+number2 = 5
+
+# Performing operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Printing the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
